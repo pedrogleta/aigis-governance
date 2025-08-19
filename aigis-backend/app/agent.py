86b6f1632f2c9ec336ab404fcd01f9f98ac7b443
Pedro Leta <pedrogleta@gmail.com>
2025-08-19 19:34:24 -0300

    start tests with langgraph in jupyter

--- aigis-backend/app/agent.py
+++ aigis-backend/app/agent.py
@@ -1,10 +1,8 @@
 from langgraph.prebuilt import create_react_agent
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import InMemorySaver
-from langgraph.graph import StateGraph
-
-checkpoointer = InMemorySaver()
-
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
 
 model = init_chat_model("deepseek-chat", model_provider="deepseek", temperature=0)
 prompt = "You are a helpful assistant"
@@ -16,5 +14,15 @@ def get_weather(city: str) -> str:
 
 
 agent = create_react_agent(model=model, tools=[get_weather], prompt=prompt)
+tools = [get_weather]
+
+# model_with_tools = model.bind_tools(tools, parallel_tool_calls=False)
+
+builder = StateGraph(MessagesState)
+
+builder.add_node("tools", ToolNode(tools))
+
+checkpoointer = InMemorySaver()
+
 
 # agent.invoke({"topic": "computers"})
