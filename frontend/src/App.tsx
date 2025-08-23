019eba045eb7f23c6d43387cc0757d7d542f8143
Pedro Leta <pedrogleta@gmail.com>
2025-08-23 19:00:48 -0300

    remove unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { apiService } from './services/api';
 import LoginPage from './pages/Login';
